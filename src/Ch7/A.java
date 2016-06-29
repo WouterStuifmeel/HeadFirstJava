84461853293e28daad13c630c8f151c14c718e09
Wouter Stuifmeel <Wouter@iMac-van-Wouter.local>
2016-06-29 23:49:26 +0200

    Chapter 7

---
+++ src/Ch7/A.java
@@ -0,0 +1,20 @@
+package Ch7;
+
+/**
+ * Created by Wouter on 29-06-16.
+ */
+public class A {
+    int ivar = 7;
+    void m1() {
+        System.out.print("A's m1, ");
+    }
+
+    void m2() {
+        System.out.print("A's m2, ");
+    }
+
+    void m3() {
+        System.out.print("A's m3, ");
+    }
+
+}
---
+++ src/Ch7/B.java
@@ -0,0 +1,10 @@
+package Ch7;
+
+/**
+ * Created by Wouter on 29-06-16.
+ */
+public class B extends A {
+    void m1() {
+        System.out.print("B's m1, ");
+    }
+}
